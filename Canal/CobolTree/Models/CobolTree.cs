c9177dbe34fe3aa37dda96c76d0c4a5bceab2f50
Jannik Arndt <jannik@jannikarndt.de>
2016-03-04 17:53:34 +0100

    Added missing files

---
+++ Canal/CobolTree/Models/CobolTree.cs
@@ -0,0 +1,31 @@
+﻿namespace Canal.CobolTree.Models
+{
+    using System;
+
+    public class CobolTree
+    {
+        public IdentificationDivision IdentificationDivision { get; set; }
+
+        public EnvironmentDivision EnvironmentDivision { get; set; }
+
+        public DataDivision DataDivision { get; set; }
+
+        public ProcedureDivision ProcedureDivision { get; set; }
+
+        public CobolTree(string sourceCode)
+        {
+            int indexIdentificationDivision = sourceCode.IndexOf("IDENTIFICATION DIVISION", StringComparison.Ordinal);
+            int indexEnvironmentDivision = sourceCode.IndexOf("ENVIRONMENT DIVISION", StringComparison.Ordinal);
+            int indexDataDivision = sourceCode.IndexOf("DATA DIVISION", StringComparison.Ordinal);
+            int indexProcedureDivision = sourceCode.IndexOf("PROCEDURE DIVISION", StringComparison.Ordinal);
+
+            this.IdentificationDivision = new IdentificationDivision(sourceCode.Substring(indexProcedureDivision, indexEnvironmentDivision - indexIdentificationDivision));
+
+            this.EnvironmentDivision = new EnvironmentDivision(sourceCode.Substring(indexEnvironmentDivision, indexDataDivision - indexEnvironmentDivision));
+
+            this.DataDivision = new DataDivision(sourceCode.Substring(indexDataDivision, indexProcedureDivision - indexDataDivision));
+
+            this.ProcedureDivision = new ProcedureDivision(sourceCode.Substring(indexProcedureDivision, sourceCode.Length - indexProcedureDivision));
+        }
+    }
+}
---
+++ Canal/CobolTree/Models/FileReference.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Canal.CobolTree.Models
+{
+    public class FileReference
+    {
+        public string FileName { get; set; }
+
+        public string FolderName { get; set; }
+
+        public CobolFile File { get; set; }
+    }
+}
